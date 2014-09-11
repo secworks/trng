eaca3c12770e94074c40d0001dbb91b10bd70bc6
Joachim Strömbergson <joachim@secworks.se>
2014-09-11 18:19:39 +0200

    Adding a lot of the control FSM.

--- src/rtl/trng_mixer.v
+++ src/rtl/trng_mixer.v
@@ -73,6 +73,7 @@ module trng_mixer(
   parameter CTRL_MIX     = 4'h2;
   parameter CTRL_SYN     = 4'h3;
   parameter CTRL_ACK     = 4'h4;
+  parameter CTRL_NEXT    = 4'h5;
   parameter CTRL_CANCEL  = 4'hf;
 
 
@@ -144,16 +145,32 @@ module trng_mixer(
   reg [31 : 0] block31_reg;
   reg [31 : 0] block31_we;
 
+  reg [4 : 0] word_ctr_reg;
+  reg [4 : 0] word_ctr_new;
+  reg         word_ctr_inc;
+  reg         word_ctr_rst;
+  reg         word_ctr_we;
+
   reg [3 : 0] mixer_ctrl_reg;
   reg [3 : 0] mixer_ctrl_new;
   reg         mixer_ctrl_we;
+
+  reg         seed_syn_reg;
+  reg         seed_syn_new;
+  reg         seed_syn_we;
+
+  reg         init_done_reg;
+  reg         init_done_new;
+  reg         init_done_we;
 
 
   //----------------------------------------------------------------
   // Wires.
   //----------------------------------------------------------------
-  reg             muxed_entropy;
+  reg [31 : 0]    muxed_entropy;
+  reg             muxed_entropy_syn;
   reg             update_block;
+  reg             mux_entropy;
 
   reg             hash_init;
   reg             hash_next;
@@ -211,39 +228,39 @@ module trng_mixer(
     begin
       if (!reset_n)
         begin
-          block00_reg <= 32'h00000000;
-          block01_reg <= 32'h00000000;
-          block02_reg <= 32'h00000000;
-          block03_reg <= 32'h00000000;
-          block04_reg <= 32'h00000000;
-          block05_reg <= 32'h00000000;
-          block06_reg <= 32'h00000000;
-          block07_reg <= 32'h00000000;
-          block08_reg <= 32'h00000000;
-          block09_reg <= 32'h00000000;
-          block10_reg <= 32'h00000000;
-          block11_reg <= 32'h00000000;
-          block12_reg <= 32'h00000000;
-          block13_reg <= 32'h00000000;
-          block14_reg <= 32'h00000000;
-          block15_reg <= 32'h00000000;
-          block16_reg <= 32'h00000000;
-          block17_reg <= 32'h00000000;
-          block18_reg <= 32'h00000000;
-          block19_reg <= 32'h00000000;
-          block20_reg <= 32'h00000000;
-          block21_reg <= 32'h00000000;
-          block22_reg <= 32'h00000000;
-          block23_reg <= 32'h00000000;
-          block24_reg <= 32'h00000000;
-          block25_reg <= 32'h00000000;
-          block26_reg <= 32'h00000000;
-          block27_reg <= 32'h00000000;
-          block28_reg <= 32'h00000000;
-          block29_reg <= 32'h00000000;
-          block30_reg <= 32'h00000000;
-          block31_reg <= 32'h00000000;
-
+          block00_reg    <= 32'h00000000;
+          block01_reg    <= 32'h00000000;
+          block02_reg    <= 32'h00000000;
+          block03_reg    <= 32'h00000000;
+          block04_reg    <= 32'h00000000;
+          block05_reg    <= 32'h00000000;
+          block06_reg    <= 32'h00000000;
+          block07_reg    <= 32'h00000000;
+          block08_reg    <= 32'h00000000;
+          block09_reg    <= 32'h00000000;
+          block10_reg    <= 32'h00000000;
+          block11_reg    <= 32'h00000000;
+          block12_reg    <= 32'h00000000;
+          block13_reg    <= 32'h00000000;
+          block14_reg    <= 32'h00000000;
+          block15_reg    <= 32'h00000000;
+          block16_reg    <= 32'h00000000;
+          block17_reg    <= 32'h00000000;
+          block18_reg    <= 32'h00000000;
+          block19_reg    <= 32'h00000000;
+          block20_reg    <= 32'h00000000;
+          block21_reg    <= 32'h00000000;
+          block22_reg    <= 32'h00000000;
+          block23_reg    <= 32'h00000000;
+          block24_reg    <= 32'h00000000;
+          block25_reg    <= 32'h00000000;
+          block26_reg    <= 32'h00000000;
+          block27_reg    <= 32'h00000000;
+          block28_reg    <= 32'h00000000;
+          block29_reg    <= 32'h00000000;
+          block30_reg    <= 32'h00000000;
+          block31_reg    <= 32'h00000000;
+          seed_syn_reg   <= 0;
           mixer_ctrl_reg <= CTRL_IDLE;
         end
       else
@@ -412,8 +429,128 @@ module trng_mixer(
             begin
               mixer_ctrl_reg <= mixer_ctrl_new;
             end
+
+          if (seed_syn_we)
+            begin
+              seed_syn_reg <= seed_syn_we;
+            end
+
+          if (init_done_we)
+            begin
+              init_done_reg <= init_done_we;
+            end
         end
     end // reg_update
+
+  //----------------------------------------------------------------
+  // entropy_mux
+  //
+  // This is a round-robin mux that muxes the signals from
+  // the entropy sources that are enabled.
+  //----------------------------------------------------------------
+  always @*
+    begin : entropy_mux
+    end // entropy_mux
+
+
+  //----------------------------------------------------------------
+  // word_mux
+  //----------------------------------------------------------------
+  always @*
+    begin : word_mux
+      block00_we = 0;
+      block01_we = 0;
+      block02_we = 0;
+      block03_we = 0;
+      block04_we = 0;
+      block05_we = 0;
+      block06_we = 0;
+      block07_we = 0;
+      block08_we = 0;
+      block09_we = 0;
+      block10_we = 0;
+      block11_we = 0;
+      block12_we = 0;
+      block13_we = 0;
+      block14_we = 0;
+      block15_we = 0;
+      block16_we = 0;
+      block17_we = 0;
+      block18_we = 0;
+      block19_we = 0;
+      block20_we = 0;
+      block21_we = 0;
+      block22_we = 0;
+      block23_we = 0;
+      block24_we = 0;
+      block25_we = 0;
+      block26_we = 0;
+      block27_we = 0;
+      block28_we = 0;
+      block29_we = 0;
+      block30_we = 0;
+      block31_we = 0;
+
+      if (update_block)
+        begin
+          case (word_ctr_reg)
+            00 : block00_we = 1;
+            01 : block01_we = 1;
+            02 : block02_we = 1;
+            03 : block03_we = 1;
+            04 : block04_we = 1;
+            05 : block05_we = 1;
+            06 : block06_we = 1;
+            07 : block07_we = 1;
+            08 : block08_we = 1;
+            09 : block09_we = 1;
+            10 : block10_we = 1;
+            11 : block11_we = 1;
+            12 : block12_we = 1;
+            13 : block13_we = 1;
+            14 : block14_we = 1;
+            15 : block15_we = 1;
+            16 : block16_we = 1;
+            17 : block17_we = 1;
+            18 : block18_we = 1;
+            19 : block19_we = 1;
+            20 : block20_we = 1;
+            21 : block21_we = 1;
+            22 : block22_we = 1;
+            23 : block23_we = 1;
+            24 : block24_we = 1;
+            25 : block25_we = 1;
+            26 : block26_we = 1;
+            27 : block27_we = 1;
+            28 : block28_we = 1;
+            29 : block29_we = 1;
+            30 : block30_we = 1;
+            31 : block31_we = 1;
+          endcase // case (word_ctr_reg)
+        end
+    end // word_mux
+
+
+  //----------------------------------------------------------------
+  // word_ctr
+  //----------------------------------------------------------------
+  always @*
+    begin : word_ctr
+      word_ctr_new = 5'h00;
+      word_ctr_we  = 0;
+
+      if (word_ctr_rst)
+        begin
+          word_ctr_new = 5'h00;
+          word_ctr_we  = 1;
+        end
+
+      if (word_ctr_inc)
+        begin
+          word_ctr_new = word_ctr_reg + 1'b1;
+          word_ctr_we  = 1;
+        end
+    end // word_ctr
 
 
   //----------------------------------------------------------------
@@ -423,8 +560,15 @@ module trng_mixer(
   //----------------------------------------------------------------
   always @*
     begin : mixer_ctrl_fsm
+      word_ctr_inc   = 0;
+      word_ctr_rst   = 0;
+      seed_syn_new   = 0;
+      seed_syn_we    = 0;
+      init_done_new  = 0;
+      init_done_we   = 0;
       hash_init      = 0;
       hash_next      = 0;
+      mux_entropy    = 0;
       update_block   = 0;
       mixer_ctrl_new = CTRL_IDLE;
       mixer_ctrl_we  = 0;
@@ -439,6 +583,9 @@ module trng_mixer(
               end
             else if (more_seed)
               begin
+                word_ctr_rst   = 1;
+                init_done_new  = 0;
+                init_done_we   = 1;
                 mixer_ctrl_new = CTRL_COLLECT;
                 mixer_ctrl_we  = 1;
               end
@@ -449,6 +596,94 @@ module trng_mixer(
             if ((!enable))
               begin
                 mixer_ctrl_new = CTRL_CANCEL;
+                mixer_ctrl_we  = 1;
+              end
+            else
+              begin
+                mux_entropy = 1;
+                if (word_ctr_reg == 5'h1f)
+                  begin
+                    mixer_ctrl_new = CTRL_MIX;
+                    mixer_ctrl_we  = 1;
+                  end
+                else
+                  begin
+                    if (muxed_entropy_syn)
+                      begin
+                        word_ctr_inc = 1;
+                      end
+                  end
+              end
+          end
+
+        CTRL_MIX:
+          begin
+            if ((!enable))
+              begin
+                mixer_ctrl_new = CTRL_CANCEL;
+                mixer_ctrl_we  = 1;
+              end
+            else
+              begin
+                if (init_done_reg)
+                  begin
+                    hash_next = 1;
+                  end
+                else
+                  begin
+                    hash_init = 1;
+                  end
+                mixer_ctrl_new = CTRL_SYN;
+                mixer_ctrl_we  = 1;
+              end
+          end
+
+        CTRL_SYN:
+          begin
+            if ((!enable))
+              begin
+                mixer_ctrl_new = CTRL_CANCEL;
+                mixer_ctrl_we  = 1;
+              end
+            else if (hash_ready)
+              begin
+                seed_syn_new   = 1;
+                seed_syn_we    = 1;
+                mixer_ctrl_new = CTRL_ACK;
+                mixer_ctrl_we  = 1;
+              end
+
+          end
+
+        CTRL_ACK:
+          begin
+            if ((!enable))
+              begin
+                mixer_ctrl_new = CTRL_CANCEL;
+                mixer_ctrl_we  = 1;
+              end
+            else if (seed_ack)
+              begin
+                seed_syn_new   = 0;
+                seed_syn_we    = 1;
+                mixer_ctrl_new = CTRL_NEXT;
+                mixer_ctrl_we  = 1;
+              end
+          end
+
+        CTRL_NEXT:
+          begin
+            if ((!enable))
+              begin
+                mixer_ctrl_new = CTRL_CANCEL;
+                mixer_ctrl_we  = 1;
+              end
+            else if (more_seed)
+              begin
+                word_ctr_rst   = 1;
+                init_done_new  = 1;
+                init_done_we   = 1;
+                mixer_ctrl_new = CTRL_COLLECT;
                 mixer_ctrl_we  = 1;
               end
           end
