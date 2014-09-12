cb77a1db3413b642fc747e331618d1cf75701bac
Joachim Strömbergson <joachim@secworks.se>
2014-09-12 11:03:24 +0200

    Adding registers and interface for three entropy sources. Removed trailing whitespace.

--- src/rtl/trng.v
+++ src/rtl/trng.v
@@ -8,30 +8,30 @@
 // Author: Joachim Strombergson
 // Copyright (c) 2014, Secworks Sweden AB
 // All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or 
-// without modification, are permitted provided that the following 
-// conditions are met: 
-// 
-// 1. Redistributions of source code must retain the above copyright 
-//    notice, this list of conditions and the following disclaimer. 
-// 
-// 2. Redistributions in binary form must reproduce the above copyright 
-//    notice, this list of conditions and the following disclaimer in 
-//    the documentation and/or other materials provided with the 
-//    distribution. 
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
-// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+//
+// Redistribution and use in source and binary forms, with or
+// without modification, are permitted provided that the following
+// conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
 // BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 //======================================================================
@@ -40,11 +40,11 @@ module trng(
             // Clock and reset.
             input wire           clk,
             input wire           reset_n,
-            
+
             // Control.
             input wire           cs,
             input wire           we,
-              
+
             // Data ports.
             input wire  [7 : 0]  address,
             input wire  [31 : 0] write_data,
@@ -54,7 +54,7 @@ module trng(
             output wire          security_error
            );
 
-  
+
   //----------------------------------------------------------------
   // Internal constant and parameter definitions.
   //----------------------------------------------------------------
@@ -62,48 +62,83 @@ module trng(
   parameter ADDR_NAME1         = 8'h01;
   parameter ADDR_VERSION       = 8'h02;
 
-  parameter CSPRNG_NUM_ROUNDS  = 8'h80;
-  
+  parameter TRNG_CTRL          = 8'h10;
+  parameter TRNG_STATUS        = 8'h11;
+
+  parameter TRNG_RND_DATA      = 8'h20;
+
+  parameter CSPRNG_NUM_ROUNDS  = 8'h30;
+  parameter CSPRNG_NUM_BLOCKS  = 8'h31;
+
+  parameter ENTROPY0_RAW       = 8'h40;
+  parameter ENTROPY1_STATS     = 8'h41;
+
+  parameter ENTROPY1_RAW       = 8'h50;
+  parameter ENTROPY1_STATS     = 8'h51;
+
+  parameter ENTROPY2_RAW       = 8'h60;
+  parameter ENTROPY2_STATS     = 8'h61;
+
   parameter TRNG_NAME0         = 32'h74726e67; // "trng"
   parameter TRNG_NAME1         = 32'h20202020; // "    "
   parameter TRNG_VERSION       = 32'h302e3031; // "0.01"
 
-  parameter CSPRNG_DEFAULT_NUM_ROUNDS = 6'h18;
-  
-  
+  parameter CSPRNG_DEFAULT_NUM_ROUNDS = 5'h18;
+  parameter CSPRNG_DEFAULT_NUM_BLOCKS = 64'h1000000000000000;
+
+
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
   //----------------------------------------------------------------
-  reg [5 : 0] csprng_num_rounds_reg;
-  reg [5 : 0] csprng_num_rounds_new;
+  reg [4 : 0] csprng_num_rounds_reg;
+  reg [4 : 0] csprng_num_rounds_new;
   reg         csprng_num_rounds_we;
 
-  
+  reg [4 : 0] csprng_num_blocks_reg;
+  reg [4 : 0] csprng_num_blocks_new;
+  reg         csprng_num_blocks_we;
+
+  reg         enable_reg;
+  reg         enable_new;
+  reg         enable_we;
+
+  reg         csprng_seed_reg;
+  reg         csprng_seed_new;
+
+
   //----------------------------------------------------------------
   // Wires.
   //----------------------------------------------------------------
 
-  reg            mixer_enable;
-
+  wire           entropy0_enable;
+  wire [31 : 0]  entropy0_raw;
+  wire [31 : 0]  entropy0_stats;
   wire           entropy0_enabled;
   wire           entropy0_syn;
   wire [31 : 0]  entropy0_data;
   wire           entropy0_ack;
 
+  wire           entropy1_enable;
+  wire [31 : 0]  entropy1_raw;
+  wire [31 : 0]  entropy1_stats;
   wire           entropy1_enabled;
   wire           entropy1_syn;
   wire [31 : 0]  entropy1_data;
   wire           entropy1_ack;
 
+  wire           entropy2_enable;
+  wire [31 : 0]  entropy2_raw;
+  wire [31 : 0]  entropy2_stats;
   wire           entropy2_enabled;
   wire           entropy2_syn;
   wire [31 : 0]  entropy2_data;
   wire           entropy2_ack;
 
+  reg            mixer_enable;
   wire [511 : 0] mixer_seed_data;
   wire           mixer_seed_syn;
   wire           mixer_seed_ack;
-  
+
   wire           csprng_enable;
   wire           csprng_debug_mode;
   wire           csprng_num_rounds;
@@ -115,20 +150,20 @@ module trng(
 
   wire           csprng_rnperror;
   wire           csprng_error;
-  
+
   wire           ctrl_rng_ack;
-  
-  reg [31 : 0] tmp_read_data;
-  reg          tmp_error;
-  
-  
+
+  reg [31 : 0]   tmp_read_data;
+  reg            tmp_error;
+
+
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
   assign read_data = tmp_read_data;
   assign error     = tmp_error;
-  
-             
+
+
   //----------------------------------------------------------------
   // core instantiations.
   //----------------------------------------------------------------
@@ -138,40 +173,40 @@ module trng(
 
                    .enable(mixer_enable),
                    .more_seed(csprng_more_seed),
-                   
+
                    .entropy0_enabled(entropy0_enabled),
                    .entropy0_syn(entropy0_syn),
                    .entropy0_data(entropy0_data),
                    .entropy0_ack(entropy0_ack),
-                   
+
                    .entropy1_enabled(entropy1_enabled),
                    .entropy1_syn(entropy1_syn),
                    .entropy1_data(entropy1_data),
                    .entropy1_ack(entropy1_ack),
-                   
+
                    .entropy2_enabled(entropy2_enabled),
                    .entropy2_syn(entropy2_syn),
                    .entropy2_data(entropy2_data),
                    .entropy2_ack(entropy2_ack),
-                   
+
                    .seed_data(mixer_seed_data),
                    .seed_syn(mixer_seed_syn),
                    .seed_ack(mixer_seed_ack)
                   );
-  
+
   trng_csprng csprng(
                      .clk(clk),
                      .reset_n(reset_n),
 
                      .enable(csprng_enable),
                      .debug_mode(csprng_debug_mode),
-                     .num_rounds(csprng_num_rounds),
-                     .num_rounds(csprng_num_blocks),
+                     .num_rounds(csprng_num_rounds_reg),
+                     .num_blocks(csprng_num_blocks_reg),
                      .seed(csprng_seed),
                      .more_seed(csprng_more_seed),
                      .ready(csprng_ready),
                      .error(csprng_error),
-                     
+
                      .seed_data(mixer_seed_data),
                      .seed_syn(mixer_seed_syn),
                      .seed_ack(csprng_seed_ack),
@@ -181,7 +216,52 @@ module trng(
                      .rnd_ack(ctrl_rng_ack)
                     );
 
-  
+  pseudo_entropy entropy0(
+                          .clk(clk),
+                          .reset_n(reset_n),
+
+                          .enable(entropy0_enable),
+
+                          .raw_entropy(entropy0_raw),
+                          .stats(entropy0_stats),
+
+                          .enabled(entropy0_enabled),
+                          .entropy_syn(entropy0_syn),
+                          .entropy_data(entropy0_data),
+                          .entropy_ack(entropy0_ack)
+                         );
+
+  avalance_entropy entropy1(
+                            .clk(clk),
+                            .reset_n(reset_n),
+
+                            .enable(entropy1_enable),
+
+                            .raw_entropy(entropy1_raw),
+                            .stats(entropy1_stats),
+
+                            .enabled(entropy1_enabled),
+                            .entropy_syn(entropy1_syn),
+                            .entropy_data(entropy1_data),
+                            .entropy_ack(entropy1_ack)
+                           );
+
+  ringosc_entropy entropy2(
+                           .clk(clk),
+                           .reset_n(reset_n),
+
+                           .enable(entropy2_enable),
+
+                           .raw_entropy(entropy2_raw),
+                           .stats(entropy2_stats),
+
+                           .enabled(entropy2_enabled),
+                           .entropy_syn(entropy2_syn),
+                           .entropy_data(entropy2_data),
+                           .entropy_ack(entropy2_ack)
+                          );
+
+
   //----------------------------------------------------------------
   // reg_update
   //
@@ -208,7 +288,7 @@ module trng(
   //----------------------------------------------------------------
   // api_logic
   //
-  // Implementation of the api logic. If cs is enabled will either 
+  // Implementation of the api logic. If cs is enabled will either
   // try to write to or read from the internal registers.
   //----------------------------------------------------------------
   always @*
@@ -218,7 +298,7 @@ module trng(
 
       tmp_read_data         = 32'h00000000;
       tmp_error             = 0;
-      
+
       if (cs)
         begin
           if (we)
@@ -230,7 +310,7 @@ module trng(
                     csprng_num_rounds_new = write_data[5 : 0];
                     csprng_num_rounds_we  = 1;
                   end
-                
+
                 default:
                   begin
                     tmp_error = 1;
@@ -251,17 +331,17 @@ module trng(
                   begin
                     tmp_read_data = TRNG_NAME1;
                   end
-                
+
                 ADDR_VERSION:
                   begin
                     tmp_read_data = TRNG_VERSION;
                   end
-                
+
                 CSPRNG_NUM_ROUNDS:
                   begin
                     tmp_read_data = {26'h0000000, csprng_num_rounds_reg};
                   end
-                
+
                 default:
                   begin
                     tmp_error = 1;
