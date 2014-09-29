7ff46f08a857ae6374286604f6ed359b3ad5ce56
Joachim Strömbergson <joachim@secworks.se>
2014-09-29 20:44:26 +0200

    Adding read and write tasks for csprng and mixer testbenches.

--- src/tb/tb_mixer.v
+++ src/tb/tb_mixer.v
@@ -97,6 +97,8 @@ module tb_mixer();
   wire           tb_syn;
   reg            tb_ack;
 
+  reg [31 : 0]  read_data;
+
 
   //----------------------------------------------------------------
   // Device Under Test.
@@ -182,6 +184,56 @@ module tb_mixer();
       $display("");
     end
   endtask // dump_dut_state
+
+
+  //----------------------------------------------------------------
+  // write_word()
+  //
+  // Write the given word to the DUT using the DUT interface.
+  //----------------------------------------------------------------
+  task write_word(input [11 : 0]  address,
+                  input [31 : 0] word);
+    begin
+      if (DEBUG)
+        begin
+          $display("*** Writing 0x%08x to 0x%02x.", word, address);
+          $display("");
+        end
+
+      tb_address = address;
+      tb_write_data = word;
+      tb_cs = 1;
+      tb_we = 1;
+      #(2 * CLK_PERIOD);
+      tb_cs = 0;
+      tb_we = 0;
+    end
+  endtask // write_word
+
+
+  //----------------------------------------------------------------
+  // read_word()
+  //
+  // Read a data word from the given address in the DUT.
+  // the word read will be available in the global variable
+  // read_data.
+  //----------------------------------------------------------------
+  task read_word(input [11 : 0]  address);
+    begin
+      tb_address = address;
+      tb_cs = 1;
+      tb_we = 0;
+      #(CLK_PERIOD);
+      read_data = tb_read_data;
+      tb_cs = 0;
+
+      if (DEBUG)
+        begin
+          $display("*** Reading 0x%08x from 0x%02x.", read_data, address);
+          $display("");
+        end
+    end
+  endtask // read_word
 
 
   //----------------------------------------------------------------
